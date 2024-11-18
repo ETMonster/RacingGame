b007da71ecf7ed506b959738e225124be6aea528
Emmett Tackaberry <emmett.tackaberry@gmail.com>
2024-11-18 10:03:36 -0500

    Camera

--- race.py
+++ race.py
@@ -10,6 +10,7 @@ class Race:
     def update_race(self, screen):
         for object_group in self.objects.to_dictionary():
             for obj in self.objects.to_dictionary()[object_group]:
+                print(obj, obj.position.x, obj.position.y)
                 obj_rotated_image = pygame.transform.rotate(obj.image, obj.rotation.angle) # Get object image and rotate it
                 obj_rect = obj_rotated_image.get_rect() # Get rect of rotated image
 
@@ -67,6 +68,25 @@ current_track = Race(
                 turn_factor = 0.5, # The rate at which cars turn
                 max_turn_speed = 3, # The maximum turn speed
                 max_speed = 600 # The maximum velocity
+            ),
+            car.Opponent(
+                image=pygame.image.load('red_car.png'),  # Car image path
+                width=pygame.image.load('red_car.png').get_width() * PIXEL_TO_SCREEN_FACTOR, # Car width so it retains its aspect with rescale
+                height=pygame.image.load('red_car.png').get_height() * PIXEL_TO_SCREEN_FACTOR, # Car height so it retains its aspect with rescale
+                position=Vector(x=0, y=0),  # Position on screen
+                rotation=Rotation(radians=0, angle=0),  # The current rotation of the car in radians and angles
+                is_player = False,
+                velocity = Vector(x=0, y=0), # Forward velocity (positive if moving forward from the car's point of view, negative if moving backwards from car's point of view)
+                direction= Vector(x=0, y=0), # The direction that the car is currently moving in (-1 to 1) and multiplies by velocity to get new position
+                gas_acceleration=50,  # Acceleration when pressing gas button
+                brake_acceleration=250,  # Acceleration when pressing brake button
+                roll_acceleration=0.98,  # Acceleration when acceleration buttons pressed
+                skid_acceleration=0.96,  # Acceleration when burning out (Target direction + Travel direction > 90)
+                min_turn_radius=30,
+                turn_factor=0.5,  # The rate at which cars turn
+                max_turn_speed=3,  # The maximum turn speed
+                max_speed=50,  # The maximum velocity
+                difficulty=1
             )
         ],
 
